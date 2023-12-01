c9351b724d8caf65fa917e49cb89c022bbf36656
emmy <52672675+emmaccode@users.noreply.github.com>
2023-12-01 13:12:47 -0700

    `Vector` `getindex` interface
    Build out the basic `getindex` base for `AlgebraVector`

--- src/Algia.jl
+++ src/Algia.jl
@@ -36,9 +36,17 @@ mutable struct Algebra{T <: Any, N <: Any} <: AbstractAlgebra
         push!(funcs, AlgebraIndex(f, 1:length, 1:N))
         new{T, N}(funcs, length)::AbstractAlgebra
     end 
-    
     Algebra{T}(f::Function = x -> 0, length::Int64 = 1, width::Int64 = 1) where T <: Any = begin
         Algebra{T, width}(x -> 0, length)::AbstractAlgebra
+    end
+    Algebra{T}(f::Function = x -> 0.0, length::Int64 = 1, width::Int64 = 1) where T <: AbstractFloat = begin
+        Algebra{T, width}(x -> 0.0, length)::AbstractAlgebra
+    end
+    Algebra{T}(f::Function = x -> true, length::Int64 = 1, width::Int64 = 1) where T <: Bool = begin
+        Algebra{T, width}(x -> true, length)::AbstractAlgebra
+    end
+    Algebra{T}(f::Function = x -> "nothing", length::Int64 = 1, width::Int64 = 1) where T <: AbstractString = begin
+        Algebra{T, width}(x -> "nothing", length)::AbstractAlgebra
     end
     Algebra{T}(f::Function, dim::Tuple) where T <: Any = begin
         if length(dim) == 1
@@ -82,7 +90,7 @@ function (:)(alg::AbstractAlgebra, f::Function)
     push!(alg.pipe, indx)
 end
 
-function (:)(alg::AbstractAlgebra, dim::Tuple{UnitRange, UnitRange}, f::Function)
+function (:)(alg::AbstractAlgebra, dim::Tuple, f::Function)
     w = typeof(alg).parameters[2]
     indx = AlgebraIndex(f, dim ...)
     push!(alg.pipe, indx)
@@ -99,9 +107,17 @@ function vect(alg::Algebra{<:Any, 1})
     gen = first(alg.pipe).f
     generated = [gen(e) for e in 1:length(alg)]
     for index in alg.pipe[2:length(alg.pipe)]
-        except = generated[index.index]
+        ind = index.index
+        except = generated[ind]
+        start = minimum(ind) - 1
+        startlen = length(except)
+        nd = maximum(ind) + 1
         index.f(except)
-        generated[index.index] .= except 
+        if length(except) != startlen
+            difference = startlen - length(except)
+            alg.length -= difference
+        end
+        generated = vcat(generated[1:start], except, generated[nd:length(generated)])
     end
     generated::AbstractArray
 end
@@ -123,7 +139,7 @@ function vect(alg::AbstractAlgebra)
     end
     generated::AbstractArray
 end
-==#
+
 function eachrow(alg::AbstractAlgebra)
 
 end
@@ -131,13 +147,42 @@ end
 function eachcol(alg::AbstractAlgebra)
 
 end
+
 function getindex(alg::AbstractAlgebra, row::UnitRange{Int64} = 1:alg.length, col::UnitRange{Int64} = 1:typeof(alg).parameters[2])
+
+end
+
+function getindex(alg::AbstractAlgebra, dim::Int64, col::Int64)
 
 end
 
 function getindex(alg::Algebra{<:Any, 1}, row::UnitRange{Int64} = 1:alg.length)
     gen = first(alg.pipe).f
-    generated = [[gen(e) for e in row for i in w]]
+    generated = [gen(e) for e in row]
+    for index in alg.pipe[2:length(alg.pipe)]
+        ind = index.index
+        except = generated[ind]
+        start = minimum(ind) - 1
+        startlen = length(except)
+        nd = maximum(ind) + 1
+        index.f(except)
+        if length(except) != startlen
+            difference = startlen - length(except)
+            alg.length -= difference
+        end
+        generated = vcat(generated[1:start], except, generated[nd:length(generated)])
+    end
+    generated::AbstractArray
+end
+
+function getindex(alg::Algebra{<:Any, 1}, dim::Int64)
+    generated = first(alg.pipe).f([dim])
+    for index in alg.pipe[2:length(alg.pipe)]
+        if generated[1] in index.index
+            index.f(generated)
+        end
+    end
+    generated
 end
 
 export AlgebraFrame, Algebra, AlgebraVector
